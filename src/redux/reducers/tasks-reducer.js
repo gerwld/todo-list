ea7686dbb48443ef1a2440c1c04513f257db736b
Patryk Jaworski <patryk.jaworski003@gmail.com>
2022-11-14 00:10:15 +0100

    Add sample data

--- src/redux/reducers/tasks-reducer.js
+++ src/redux/reducers/tasks-reducer.js
@@ -1,6 +1,11 @@
 const init = {
  isInitialized: false,
- tasks: []
+ tasks: [
+  {id: '59h9bdf9n434', title: 'Task Title', desc: 'Task Description', 
+  dateCreated: '1668380951687.2', lastEdit: '1668380951687.2',
+  labels: ['some lable', 'work'], 
+  subtasks: [{id: '49bkfdke4', title: 'Subtask 1', isDone: false}, {id: '49bkfdkfe4', title: 'Subtask 2', isDone: false}]}
+ ]
 };
 
 const tasksReducer = (state = init, action) => {
