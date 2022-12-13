e836543efcf8379d1e055935407cb775bc4ee3a5
Patryk Jaworski <patryk.jaworski003@gmail.com>
2022-12-13 23:42:00 +0100

    Created toSentenceCase

---
+++ src/tools/toSentenceCase.js
@@ -0,0 +1,32 @@
+function toSentenceCase(text) {
+ let symbs = [".", ",", ";", "!", "?"],
+  res = [];
+
+ //basic checks
+ if (!text && text !== 0) return "Wrong argument";
+ else text = text.toString();
+
+ //if string contains symbol split, then map with strClearAndFrUpper
+ for (let sym of symbs) {
+  if (text.indexOf(sym) !== -1) {
+   res = text.split(sym).map((e) => {
+    return strClearAndFrUpper(e + sym);
+   });
+  }
+ }
+ // if doesn't contain any from [symbs]
+ if (res.length === 0) res = strClearAndFrUpper(text + ".");
+
+ return typeof res === "object" ? res.join(" ") : res;
+}
+
+function strClearAndFrUpper(str) {
+ str = str
+  .toString()
+  .split(" ")
+  .filter((e) => e !== "")
+  .join(" ");
+ return str.charAt(0).toUpperCase() + str.substring(1);
+}
+
+export default toSentenceCase;
